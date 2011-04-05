279846a2c001f0a4d4cad8c81b7adf47bdd87748
Jori Hardman <jorihardman@gmail.com>
2011-04-05 16:50:30 -0500

    initial commit with users and some gems

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.3'
+gem 'mysql2'
+gem 'jquery-rails'
+gem 'client_side_validations'
+gem 'dynamic_form'
+gem 'will_paginate'
+gem 'authlogic'
+gem 'mail'
+gem 'exception_notification'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neighborly::Application.config.secret_token = 'ae7b1dd0b9d2c8101b74cef387d0a7353713176bed3661c212da24165f10abdf2180454b7e812709d2b19806569f3ad5d966d20936d998d817c85307959a50c0'
