377dd557f236bac97cb1a47cf30f7feb59dee950
Jori Hardman <jorihardman@gmail.com>
2011-04-26 15:57:02 -0500

    Changed nav helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,46 +8,25 @@ module ApplicationHelper
   end
 
   def sidenav_content
-    if action_name =~ /inventory/
-      inv_class = 'current'
-      req_class = ''
-    else
-      inv_class = ''
-      req_class = 'current'
-    end
+    inv_class = (action_name =~ /inventory/) ? 'current' : ''
+    inv_path = (action_name =~ /services/) ? services_inventory_postings_path : products_inventory_postings_path
 
-    if action_name =~ /services/
-      inv_path = services_inventory_postings_path
-      req_path = services_requests_postings_path
-    else
-      inv_path = products_inventory_postings_path
-      req_path = products_requests_postings_path
-    end
+    req_class = (action_name =~ /requests/) ? 'current' : ''
+    req_path = (action_name =~ /services/) ? services_requests_postings_path : products_requests_postings_path
 
-    return '<li>' << link_to('Inventory',
-                          inv_path,
-                          :class => inv_class) <<
-           '</li><li>' << link_to('Requests',
-                               req_path,
-                               :class => req_class) << '</li>'
+    return "<li>#{link_to('Inventory', inv_path, :class => inv_class)}</li>" <<
+           "<li>#{link_to('Requests',req_path, :class => req_class)}</li>"
   end
 
   def topnav_content
-    if action_name =~ /services/
-      serv_class = 'current'
-      prod_class = ''
-    else
-      serv_class = ''
-      prod_class = 'current'
-    end
+    prod_class = (action_name =~ /products/) ? 'current' : ''
+    prod_path = (action_name =~ /inventory/) ? products_inventory_postings_path : products_requests_postings_path
 
-    return '<li>' << link_to('Products',
-                            products_requests_postings_path,
-                            :class => prod_class) << '</li>' <<
-           '<li>' << link_to('Services',
-                            services_requests_postings_path,
-                            :class => serv_class) << '</li>' <<
-           '<li><a href="#">My Stuff</a></li>'
+    serv_class = (action_name =~ /services/) ? 'current' : ''
+    serv_path = (action_name =~ /inventory/) ? services_inventory_postings_path : services_requests_postings_path
+
+    return "<li>#{link_to('Products', prod_path, :class => prod_class)}</li>" <<
+           "<li>#{link_to('Services', serv_path, :class => serv_class)}</li>"
   end
 end
 
