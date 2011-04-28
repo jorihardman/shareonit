384eedbf3565c55ff960343d931929a3d3c7464a
Jori Hardman <jorihardman@gmail.com>
2011-04-28 16:35:43 -0500

    Made adding offered items to inventory a class method

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -3,5 +3,12 @@ class Posting < ActiveRecord::Base
   has_many :offers
 
   default_scope select('posting.*, user.full_name').includes(:user)
+
+  def self.add_to_inventory(offer)
+    invPosting = offer.posting.clone
+    invPosting.have_need = 'have'
+    invPosting.user_id = offer.user_id
+    invPosting.save
+  end
 end
 
