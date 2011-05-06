26b331ad942e803ea117dc84912f3e3f595a3f45
Jori Hardman <jorihardman@gmail.com>
2011-05-05 22:13:36 -0500

    adjusted theme and added jquery-ui theme

--- app/helpers/postings_helper.rb
+++ app/helpers/postings_helper.rb
@@ -4,14 +4,14 @@ module PostingsHelper
     have_need = action_name =~ /inventory/ ? 'have' : 'need'
     link_to 'Create New Posting',
       new_posting_path(:product_service => product_service, :have_need => have_need),
-      :rel => 'facebox'
+      :rel => 'facebox', :class => 'button'
   end
 
   def posting_table_row(posting)
     "<td>#{posting.description}</td>" <<
     "<td>#{posting.from_date}</td>" << "<td>#{posting.to_date}</td>" <<
     "<td>#{posting.user.full_name}</td>" <<
-    "<td>#{link_to 'Edit', edit_posting_path(posting), :rel => 'facebox'}</td>"
+    "<td>#{link_to 'Edit', edit_posting_path(posting), :rel => 'facebox', :class => 'button'}</td>"
   end
 
 end
