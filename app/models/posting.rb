e41d46031eae22a84e52c2f971b0c76c27e759e2
Jori Hardman <jorihardman@gmail.com>
2011-05-17 16:42:22 -0500

    Minor changes to search

--- app/models/posting.rb
+++ app/models/posting.rb
@@ -16,11 +16,11 @@ class Posting < ActiveRecord::Base
   def self.search_or_where(search, condition, page)
     if search
       return Posting.where(condition).where(
-        "((first_name || ' ' || last_name) ILIKE ? OR description ILIKE ?) AND deleted = ?", 
-        "%#{search}%", "%#{search}%", false
+        "((first_name || ' ' || last_name) ILIKE ? OR description ILIKE ?) AND deleted = false", 
+        "%#{search}%", "%#{search}%"
       ).order('postings.created_at DESC').paginate(:page => page, :per_page => 15)
     else
-      return Posting.where(condition).where(:deleted => false).order('postings.created_at DESC').paginate(:page => page, :per_page => 15)
+      return Posting.where(condition).where('deleted = false').order('postings.created_at DESC').paginate(:page => page, :per_page => 15)
     end
   end
 end
